44ce6df47184e328b87fa10fd4d89c5c5487423f
kelvin Musselli <kelvinmusselli22@gmail.com>
2022-04-23 16:38:03 -0300

    feat: validate if category exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,6 +13,12 @@ categoriesRoutes.get('/', (request, response) => {
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists' });
+  }
+
   categoryRepository.create({ name, description });
 
   return response.status(201).send();
