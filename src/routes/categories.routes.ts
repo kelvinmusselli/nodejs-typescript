672e3a42d47d972d0dab38fc3b5259730f87d877
kelvin Musselli <kelvinmusselli22@gmail.com>
2022-04-23 16:28:53 -0300

    feat: list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,12 +5,17 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 const categoriesRoutes = Router();
 const categoryRepository = new CategoriesRepository();
 
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoryRepository.list();
+  return response.status(200).json(categories);
+});
+
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
   categoryRepository.create({ name, description });
 
-  return response.status(201);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
