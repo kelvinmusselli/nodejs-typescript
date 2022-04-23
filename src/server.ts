5261223cfe3a68afc0de726a3e53e32dd4ed770d
kelvin Musselli <kelvinmusselli22@gmail.com>
2022-04-23 15:10:08 -0300

    feat: add diagram

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
-
-app.listen(3333, () => {
-  console.log("Server started on port 3333!");
-});
+app.listen(3333, () => console.log("Server started on port 3333!"));
